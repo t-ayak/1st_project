4e63e94f912de32ec7300a681eb13c9564d05035
Kobayashi Ayaka <ayaka.kobayashi156@unideb.mailbox.hu>
2026-04-02 20:00:34 +0900

    timeline仮完成

---
+++ script.js
@@ -0,0 +1,19 @@
+function startQuiz(){
+    let answer = prompt("解答を入力してください");
+
+    if (answer) {
+        answer = answer.trim().toLowerCase();
+    }
+
+    if (answer === "familymart") {
+        alert("正解！忘れて欲しいまであるけどね。");
+        window.location.href = "memories.html";
+    
+    } else if (answer === "blackthomasism") {
+        alert("ポストカード届いてよかった！隠しページに行きマース！");
+        window.location.href = "message.html"; 
+
+    } else {
+        alert("不正解。ヒントは瀧田のバイト先");
+    }
+}
